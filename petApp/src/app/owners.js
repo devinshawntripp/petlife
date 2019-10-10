c597b1dbe5eaba4bd165219e130d33872353379d
Clint Wyatt <ClintWyatt@my.unt.edu>
2019-10-10 12:43:14 -0500

    Adding a protion of the owners object to the app

---
+++ petApp/src/app/owners.js
@@ -0,0 +1,49 @@
+class Owner
+{
+	constructor(name)
+	{
+		this._name = name;
+		this._owners = [];
+	}
+	set id(x)
+	{
+		this._id = x;
+	}
+	set name(name)
+	{
+		this._name = name;
+	}
+	set houseHold(house)
+	{
+		this._household = house;
+	}
+	get name()
+	{
+		return this._name;
+	}
+	get id()
+	{
+		return this._id;
+	}
+	get houseHold()
+	{
+		return this._household;
+	}
+	addOwner(name)
+	{
+		this._owners.push(name);
+	}
+	deleteOwner(name)
+	{
+		for(let i =0; i < _owners.length; i++)
+		{
+			//if the owner is found, delete it from the array of other owners
+		}
+	}
+	removeFromHouse()
+	{
+		this._household = " ";//setting the household to a empty string
+	}
+
+
+}
