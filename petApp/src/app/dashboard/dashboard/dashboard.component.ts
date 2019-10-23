af0830edbc4d54e5ef728ad193ff753e858e9161
sarah <sarahricha98@gmail.com>
2019-10-23 17:27:22 -0500

    updated dashboard

--- petApp/src/app/dashboard/dashboard/dashboard.component.ts
+++ petApp/src/app/dashboard/dashboard/dashboard.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  
+  public person: string
 
   constructor() { }
 
   ngOnInit() {
+    this.person = "Sarah"
   }
 
 }
