a0354044e995e6c4555dfccbc9195a014366c8bb
jlh0798 <jlhern@DESKTOP-JD978QS.localdomain>
2019-10-30 14:00:54 -0500

    Removing leftover reference to 'needs'

--- petApp/src/app/pets/pet/pet.component.ts
+++ petApp/src/app/pets/pet/pet.component.ts
@@ -1,12 +1,12 @@
 
-import { Need } from '../../needs/need.component'
+/**import { Need } from '../../needs/need.component'**/
 export class Pet {
 
   constructor(
     public id: number,
     public name: string,
     public description: string,
-    public Needs: Array<Need>
+/**    public Needs: Array<Need>**/
   ) { }
 
 }
