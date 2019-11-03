8de152e1552334becdeeec796392708872492e9c
sarah <sarahricha98@gmail.com>
2019-11-02 19:00:04 -0500

    can now delete a pet

--- petApp/src/app/pets/delete-pet/delete-pet/delete-pet.component.ts
+++ petApp/src/app/pets/delete-pet/delete-pet/delete-pet.component.ts
@@ -1,17 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-delete-pet',
   templateUrl: './delete-pet.component.html',
   styleUrls: ['./delete-pet.component.css']
 })
-export class DeletePetComponent{
+export class DeletePetComponent implements OnInit{
 
-  constructor(public dialogRef: MatDialogRef<DeletePetComponent>) { }
+  pets: Array<any>
+
+  constructor(
+    public dialogRef: MatDialogRef<DeletePetComponent>,
+    public firebaseService: FirebaseService,
+    private router: Router
+    ) { }
 
   confirmSelection() {
     this.dialogRef.close();
+  }
+
+  ngOnInit(){
+    this.getPets()
+  }
+
+  getPets(){
+    this.firebaseService.getPets()
+    .subscribe(result => {
+      this.pets = result;
+    })
+  }
+
+  deletePet(pet){
+    this.firebaseService.deletePet(pet)
   }
 
 }
